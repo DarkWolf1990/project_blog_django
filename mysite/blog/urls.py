2bb41a83be174504a82c9b32b5521bb61daee5cc
darkwolf <darkwolf1990janary@gmail.com>
2023-11-20 20:27:48 +0300

    settings templates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 
-urlpatterns =[
-    # Представление поста
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
-]
+urlpatterns = [
+                  # Представление поста
+                  path('', views.post_list, name='post_list'),
+                  path('<int:id>/', views.post_detail, name='post_detail'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
